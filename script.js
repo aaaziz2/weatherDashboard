3d18664a2aa90d84888c556ca7c240861d27570e
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-17 16:49:44 -0500

    Added Bootstrap and skeleton for today's weather and 5 day forecast Commented out console logging for APIs to get city lat/lon and making sure string manipulation is working Assigning values from One Call API for current weather zeroing out city name after search is submitted created variables to access current weather and for forecast

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var city = document.querySelector("#city")
 var weatherSearch = document.querySelector("#weatherSearch")
+var today = document.querySelector("#today")
+var forecast = document.querySelector("#forecast")
 
 var oldUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
 
@@ -13,21 +15,21 @@ var curLon
 
 function dashGenerate(event){
     event.preventDefault()
-    oldUrl = oldUrl.replace("{city name}",city.value)
-    console.log(oldUrl)
+    // oldUrl = oldUrl.replace("{city name}",city.value)
+    // console.log(oldUrl)
 
     latLonGetter = latLonGetter.replace("{city name}",city.value)
-    console.log(latLonGetter)
+    // console.log(latLonGetter)
 
     fetch(latLonGetter)
       .then(function (response) {
         return response.json()
       })
       .then(function (data) {
-        console.log(data)
+        // console.log(data)
         curLat = data[0].lat
         curLon = data[0].lon
-        console.log(curLat + ' ' + curLon)
+        // console.log(curLat + ' ' + curLon)
 
         url = url.replace("{lat}", curLat)
         url = url.replace("{lon}", curLon)
@@ -40,9 +42,18 @@ function dashGenerate(event){
             })
             .then(function (data) {
                 console.log(data)
+                console.log(data.current.temp)
+                today.children[1].textContent = `Temp: ${data.current.temp} F`
+                console.log(data.current.wind_speed)
+                today.children[2].textContent = `Wind: ${data.current.wind_speed} MPH`
+                console.log(data.current.humidity)
+                today.children[3].textContent = `Humidity ${data.current.humidity} %`
+                console.log(data.current.uvi)
+                today.children[4].textContent = `UV Index ${data.current.uvi}` 
             })  
     })
-        
+
+    city.value = ""
 }
 
 weatherSearch.addEventListener('submit', dashGenerate)
