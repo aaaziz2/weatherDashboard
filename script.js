ae79de7cdd2e9ddd4b04dc179b0463ddb0a66159
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-17 09:54:37 -0500

    Had slight typo for lon replacement Found that that two fetch requests happened simultaneously nested the second fetch request within the first working now!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var city = document.querySelector("#city")
 var weatherSearch = document.querySelector("#weatherSearch")
 
-var oldUrl = "https://api.openweathermap.org/data/2.5/onecall?q={city name}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
+var oldUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
 
-var url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=alerts,hourly,minutely&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
+var url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
 
 var latLonGetter = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=820ed8269e09bb46f1883762931ddaef"
 
@@ -28,20 +28,21 @@ function dashGenerate(event){
         curLat = data[0].lat
         curLon = data[0].lon
         console.log(curLat + ' ' + curLon)
-        
-        url = url.replace("{lat}", curLat)
-        url = url.replace("{lon", curLon)
-    })
-    
-    console.log(url)
 
-    fetch(url)
-      .then(function (response) {
-        return response.json()
-      })
-      .then(function (data) {
-        console.log(data)
-    })  
+        url = url.replace("{lat}", curLat)
+        url = url.replace("{lon}", curLon)
+
+        console.log(url)
+
+        fetch(url)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+            })  
+    })
+        
 }
 
 weatherSearch.addEventListener('submit', dashGenerate)
