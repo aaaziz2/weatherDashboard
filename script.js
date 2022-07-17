3e730704c3c188abb7163137ff0a3778e6c270fe
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-17 09:46:17 -0500

    Added new urls for different OepnWeather APIs The OneCall API has everything we need but requires latitude and longitude There is a geocoding API from OpenWeather Grabbed the lat and longitude from geocoding For some reason its not replacing the query like it should

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 var city = document.querySelector("#city")
 var weatherSearch = document.querySelector("#weatherSearch")
 
-var url = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=820ed8269e09bb46f1883762931ddaef"
+var oldUrl = "https://api.openweathermap.org/data/2.5/onecall?q={city name}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
+
+var url = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=alerts,hourly,minutely&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
+
+var latLonGetter = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=820ed8269e09bb46f1883762931ddaef"
+
+var curLat
+var curLon
+
 
 function dashGenerate(event){
     event.preventDefault()
-    url = url.replace("{city name}",city.value)
+    oldUrl = oldUrl.replace("{city name}",city.value)
+    console.log(oldUrl)
+
+    latLonGetter = latLonGetter.replace("{city name}",city.value)
+    console.log(latLonGetter)
+
+    fetch(latLonGetter)
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (data) {
+        console.log(data)
+        curLat = data[0].lat
+        curLon = data[0].lon
+        console.log(curLat + ' ' + curLon)
+        
+        url = url.replace("{lat}", curLat)
+        url = url.replace("{lon", curLon)
+    })
+    
     console.log(url)
 
     fetch(url)
@@ -14,11 +41,7 @@ function dashGenerate(event){
       })
       .then(function (data) {
         console.log(data)
-        console.log(data.main.temp)
-        
-    
-    })
-      
+    })  
 }
 
 weatherSearch.addEventListener('submit', dashGenerate)
