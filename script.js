581b2e27eebb4151cc5538cb406d0acc38276840
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-17 22:10:11 -0500

    forecast cards are hidden by default in style.css added acceptence criteria to README added some comments to explain some of the HTML added div for displaying past searched cities, not implemented yet manually added elements to the card group before it would add more cards, this way it just replaces the data from before added comments to explain most of the JS added variables so that you could run the search multiple times and get the correct info removed old uncessary code

--- script.js
+++ script.js
@@ -1,103 +1,106 @@
+// Access HTML Elements
 var city = document.querySelector("#city")
 var weatherSearch = document.querySelector("#weatherSearch")
 var today = document.querySelector("#today")
 var forecast = document.querySelector("#forecast")
 var forecastTitle = document.querySelector("#forecastTitle")
-
+// Today's Date with Moment
 var todayDate = moment()
-
-// var oldUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
+// APIs for One Call 1.0, the Geocoding API, and their weather icon database
 var url = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=820ed8269e09bb46f1883762931ddaef&units=imperial"
 var latLonGetter = "http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=820ed8269e09bb46f1883762931ddaef"
 var iconURL = "http://openweathermap.org/img/w/{icon}.png"
-
+// variables for Longitude and Latitude
 var curLat
 var curLon
-
+// variables to cycle thru different cities
+var lastCity = "{city name}"
+var lastLat = "{lat}"
+var lastLon = "{lon}"
+// Generate Weather Info
 function dashGenerate(event){
+    // https://www.javascripttutorial.net/web-apis/javascript-localstorage/
+    // get list of names from localStorage
+    let keys = Object.keys(localStorage)
+    // stop if no city is entered
     if(city.value == ''){
       return
     }
+    // Stop page from refreshing
     event.preventDefault()
-    // oldUrl = oldUrl.replace("{city name}",city.value)
-    // console.log(oldUrl)
+    // add City to localStorage
     localStorage.setItem(city.value, city.value)
-    latLonGetter = latLonGetter.replace("{city name}",city.value)
-    // console.log(latLonGetter)
-
+    // Search for current city, replace text with default or the last city entered
+    latLonGetter = latLonGetter.replace(lastCity, city.value)
+    // set current city as the past city
+    lastCity = city.value
+    console.log(latLonGetter)
+    // run geocoding API to get lat/lon
     fetch(latLonGetter)
       .then(function (response) {
         return response.json()
       })
       .then(function (data) {
         // console.log(data)
+        // set current lat & lon
         curLat = data[0].lat
         curLon = data[0].lon
         // console.log(curLat + ' ' + curLon)
-
-        url = url.replace("{lat}", curLat)
-        url = url.replace("{lon}", curLon)
-
+        // replace last lat/long or default with current one pulled from API
+        url = url.replace(lastLat, curLat)
+        url = url.replace(lastLon, curLon)
+        // set current lat/lon as the the past lat/lon
+        lastLat = curLat
+        lastLon = curLon
         console.log(url)
-
+        // get weather data based on lat/lon
         fetch(url)
             .then(function (response) {
                 return response.json()
             })
             .then(function (data) {
                 console.log(data)
-                
+                // Set Weather Info based on info pulled from One Call 1.0 API
                 today.children[0].textContent = `${city.value} (${todayDate.format('MM/DD/YYYY')})`
-                // console.log(data.current.temp)
                 today.children[1].textContent = `Temp: ${data.current.temp} F`
-                // console.log(data.current.wind_speed)
                 today.children[2].textContent = `Wind: ${data.current.wind_speed} MPH`
-                // console.log(data.current.humidity)
                 today.children[3].textContent = `Humidity ${data.current.humidity} %`
-                // console.log(data.current.uvi)
+                // need to figure out formatting for UV Index
                 today.children[4].textContent = `UV Index ${data.current.uvi}`
-                
+                // Reset the search bar
                 city.value = ""
-
+                // Add Title for Forecast
                 forecastTitle.children[0].textContent = "5-Day Forecast:"
+                // Display the cards
+                forecast.style = "display:flex"
+                // Loop thru 5 cards
                 for(var i = 0; i < 5; i++){
-                  
+                  // grab date, it's provided in UNIX time
                   var forecastDate = moment.unix(data.daily[i].dt)
-
-                  var forecastCard = document.createElement('div')
-                  var title = document.createElement('h4')
-                  var icon = document.createElement('img')
-                  var forecastTemp = document.createElement('p')
-                  var forecastWind = document.createElement('p')
-                  var forecastHum = document.createElement('p')
-
-                  forecastCard.classList.add("card")
-                  
+                  // grab elements from HTML
+                  var title = forecast.children[i].children[0]
+                  var icon = forecast.children[i].children[1]
+                  var forecastTemp = forecast.children[i].children[2]
+                  var forecastWind = forecast.children[i].children[3]
+                  var forecastHum = forecast.children[i].children[4]
+                  // set title & class
                   title.textContent = forecastDate.format("MM/DD/YYYY")
                   title.classList.add("card-title")
-                  forecastCard.appendChild(title)
-
-                  icon.src = iconURL.replace("{icon}",data.daily[i].weather[0].icon)
-                  forecastCard.appendChild(icon)                  
-
+                  // set icon
+                  icon.src = iconURL.replace("{icon}",data.daily[i].weather[0].icon)    
+                  // set temp
                   forecastTemp.textContent = `Temp: ${data.daily[i].temp.day} F`
                   forecastTemp.classList.add("card-text")
-                  forecastCard.appendChild(forecastTemp)
-
+                  // set wind speed
                   forecastWind.textContent = `Wind: ${data.daily[i].wind_speed} MPH`
                   forecastWind.classList.add("card-text")
-                  forecastCard.appendChild(forecastWind)
-
+                  // set humidity
                   forecastHum.textContent = `Humidity ${data.daily[i].humidity} %`
                   forecastHum.classList.add("card-text")
-                  forecastCard.appendChild(forecastHum)
-                  
-                  forecast.appendChild(forecastCard)
                 }
             })  
     })
-
     
 }
-
+// if City name is submitted generate dashboard
 weatherSearch.addEventListener('submit', dashGenerate)
